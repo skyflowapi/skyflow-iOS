bb643389a6ff6dd1e8c3228d4d849c5bc1088fe3
akhil-skyflow <akhil.mangala@skyflow.com>
2021-08-27 18:35:15 +0530

    Minor contract changes and pure collect/reveal implementation fixes

--- Sources/Skyflow/core/protocols/Callback.swift
+++ Sources/Skyflow/core/protocols/Callback.swift
@@ -5,7 +5,7 @@
 //  Created by Akhil Anil Mangala on 20/07/21.
 //
 public protocol Callback {
-    func onSuccess(_ responseBody: String) -> Void
+    func onSuccess(_ responseBody: Any) -> Void
     func onFailure(_ error: Error) -> Void
 }
 
