5d3941b2b233af99b6589ef75d1195d340d2b03b
Tejesh Reddy <tejesh.allampati@skyflow.com>
2021-09-16 15:47:28 +0530

    Imported Foundation

--- Example/Example/ExampleAPICallback.swift
+++ Example/Example/ExampleAPICallback.swift
@@ -1,3 +1,4 @@
+import Foundation
 import Skyflow
 
 public class ExampleAPICallback: Skyflow.Callback {
--- Example/Example/ExampleTokenProvider.swift
+++ Example/Example/ExampleTokenProvider.swift
@@ -1,3 +1,4 @@
+import Foundation
 import Skyflow
 
 public class ExampleTokenProvider : TokenProvider {
