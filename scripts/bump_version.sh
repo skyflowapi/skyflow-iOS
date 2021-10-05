da0c65a5fe112ab457ce8b8242bd72cd126faabf
Tejesh Reddy <tejesh.allampati@skyflow.com>
2021-10-05 17:33:31 +0530

    fixed public release

--- scripts/bump_version.sh
+++ scripts/bump_version.sh
@@ -3,19 +3,21 @@
 version=$1
 SEMVER=$version
 
-if [ -z $3 ]
+if [ -z $2 ]
 then
     sed -E "s/spec.version .+/spec.version      = \"$SEMVER\"/g" "./Skyflow.podspec" > tempfile
+    sed -E "s/spec.source       = .+/spec.source       = { :git => \"https://github.com/skyflowapi/skyflow-iOS.git\", :tag => \"$1\" }" tempfile > "./Skyflow.podspec" && rm -f tempfile
 
     echo --------------------------
     echo "Done, Pod now at v$1"
 
 else
     sed -E "s/spec.version .+/spec.version      = \"$SEMVER-dev.$2\"/g" "./Skyflow.podspec" > tempfile
+    sed -E "s/spec.source       = .+/spec.source       = { :git => \"https://github.com/skyflowapi/skyflow-iOS.git\", :commit => \"$2\" }" tempfile > "./Skyflow.podspec" && rm -f tempfile
     
     echo --------------------------
     echo "Done, Pod now at v$1-dev.$2"
 fi
 
-    sed -E "s/:commit => \".*\"/:commit => \"$2\"/g" tempfile > "./Skyflow.podspec" && rm -f tempfile
+        
 
