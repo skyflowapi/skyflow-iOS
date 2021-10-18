c1d9ee8d6717ea4b06ba64bb9832b98155f2a213
akhil-skyflow <akhil.mangala@skyflow.com>
2021-10-18 20:30:09 +0530

    Deprecate redaction in reveal input

--- Sources/Skyflow/reveal/RevealElementInput.swift
+++ Sources/Skyflow/reveal/RevealElementInput.swift
@@ -16,6 +16,7 @@ public struct RevealElementInput {
     internal var redaction: RedactionType
     internal var altText: String?
 
+    @available(*, deprecated, message: "redaction param is deprecated")
     public init(token: String = "", inputStyles: Styles? = Styles(), labelStyles: Styles? = Styles(), errorTextStyles: Styles? = Styles(), label: String, redaction: RedactionType = .DEFAULT, altText: String? = nil) {
         self.token = token
         self.inputStyles = inputStyles
@@ -23,6 +24,16 @@ public struct RevealElementInput {
         self.errorTextStyles = errorTextStyles
         self.label = label
         self.redaction = redaction
+        self.altText = altText
+    }
+    
+    public init(token: String = "", inputStyles: Styles? = Styles(), labelStyles: Styles? = Styles(), errorTextStyles: Styles? = Styles(), label: String, altText: String? = nil) {
+        self.token = token
+        self.inputStyles = inputStyles
+        self.labelStyles = labelStyles
+        self.errorTextStyles = errorTextStyles
+        self.label = label
+        self.redaction = .DEFAULT
         self.altText = altText
     }
 }
