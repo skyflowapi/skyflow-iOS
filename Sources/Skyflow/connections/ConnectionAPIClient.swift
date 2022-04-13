1c22280395fde9cd86cb3e3ebd0072f16321e1f4
Tejesh Reddy <tejesh.allampati@skyflow.com>
2022-04-13 09:43:16 +0530

    [SDK-519] Refactor connection api client

--- Sources/Skyflow/connections/ConnectionAPIClient.swift
+++ Sources/Skyflow/connections/ConnectionAPIClient.swift
@@ -15,19 +15,13 @@ class ConnectionAPIClient {
 
         var isSuccess = true
         var errorObject: Error!
+        
         var convertedResponse: [String: Any]?
         var stringResponse: String?
         var errors: [NSError] = []
         
         do {
-            let url = try RequestHelpers.createRequestURL(baseURL: config.connectionURL, pathParams: config.pathParams, queryParams: config.queryParams, contextOptions: self.contextOptions)
-            var request = try RequestHelpers.createRequest(url: url, method: config.method, body: config.requestBody, headers: config.requestHeader, contextOptions: self.contextOptions)
-            
-            if !(RequestHelpers.getLowerCasedHeaders(headers: request.allHTTPHeaderFields).keys.contains("x-skyflow-authorization") ){
-                request.setValue(token, forHTTPHeaderField: "x-skyflow-authorization")
-            }
-
-            let session = URLSession(configuration: .default)
+            let (request, session) = try getRequestSession(config: config, token: token)
 
             connectionRequestGroup.enter()
 
@@ -35,78 +29,101 @@ class ConnectionAPIClient {
                 defer {
                     connectionRequestGroup.leave()
                 }
-
-                if error != nil {
+                do {
+                    (convertedResponse, errors, stringResponse) = try self.processResponse(data: data, response: response, error: error, config: config)
+                    if !errors.isEmpty {
+                        isSuccess = false
+                        errorObject = nil
+                    }
+                } catch {
                     isSuccess = false
-                    errorObject = error!
+                    errorObject = error
                     return
                 }
 
-                if let httpResponse = response as? HTTPURLResponse {
-                    let range = 400...599
-                    if range ~= httpResponse.statusCode {
-                        var desc = "Invoke Connection call failed with the following status code " + String(httpResponse.statusCode)
 
-                        if let safeData = data {
-                            desc = String(decoding: safeData, as: UTF8.self)
-                        }
-
-                        isSuccess = false
-                        errorObject = ErrorCodes.APIError(code: httpResponse.statusCode, message: desc).getErrorObject(contextOptions: self.contextOptions)
-                        return
-                    }
-                }
-
-
-                if let safeData = data {
-                    do {
-                        let responseData = try JSONSerialization.jsonObject(with: safeData, options: .allowFragments)
-                        if responseData is [String: Any] {
-                            convertedResponse = try RequestHelpers.parseActualResponseAndUpdateElements(
-                                response: responseData as! [String: Any],
-                                responseBody: config.responseBody ?? [:],
-                                contextOptions: self.contextOptions)
-                            errors = RequestHelpers.getInvalidResponseKeys(config.responseBody ?? [:], responseData as! [String : Any], contextOptions: self.contextOptions)
-                            if !errors.isEmpty {
-                                isSuccess = false
-                                errorObject = nil
-                            }
-                        } else if responseData is String {
-                            stringResponse = responseData as? String
-                        }
-                    } catch {
-                        isSuccess = false
-                        errorObject = error
-                        return
-                    }
-                }
             }
             task.resume()
 
             connectionRequestGroup.notify(queue: .main) {
-                if isSuccess {
-                    do {
-                        if convertedResponse != nil {
-                            let sanitizedResponse = try ConversionHelpers.removeEmptyValuesFrom(response: convertedResponse!, contextOptions: self.contextOptions)
-
-                            self.callback.onSuccess(sanitizedResponse)
-                        } else if stringResponse != nil {
-                            self.callback.onSuccess(stringResponse!)
-                        }
-                    } catch {
-                        self.callback.onFailure(error)
-                    }
-                } else {
-                    if !errors.isEmpty {
-                        let failureResponse: [String: Any] = ["success": convertedResponse ?? [:], "errors": [errors]]
-                        self.callback.onFailure(failureResponse)
-                    } else {
-                        self.callback.onFailure(["errors": [errorObject!]])
-                    }
-                }
+                self.handleCallbacks(isSuccess: isSuccess, convertedResponse: convertedResponse, stringResponse: stringResponse, errors: errors, errorObject: errorObject)
             }
         } catch {
             throw error
+        }
+    }
+    
+    func getRequestSession(config: ConnectionConfig, token: String) throws -> (URLRequest, URLSession){
+        let url = try RequestHelpers.createRequestURL(baseURL: config.connectionURL, pathParams: config.pathParams, queryParams: config.queryParams, contextOptions: self.contextOptions)
+        var request = try RequestHelpers.createRequest(url: url, method: config.method, body: config.requestBody, headers: config.requestHeader, contextOptions: self.contextOptions)
+        
+        if !(RequestHelpers.getLowerCasedHeaders(headers: request.allHTTPHeaderFields).keys.contains("x-skyflow-authorization") ){
+            request.setValue(token, forHTTPHeaderField: "x-skyflow-authorization")
+        }
+
+        return (request, URLSession(configuration: .default))
+    }
+    
+    func processResponse(data: Data?, response: URLResponse?, error: Error?, config: ConnectionConfig) throws -> ([String: Any]?, [NSError], String?){
+        
+        var convertedResponse: [String: Any]?
+        var stringResponse: String?
+        var errors: [NSError] = []
+        
+        if error != nil {
+            throw error!
+        }
+
+        if let httpResponse = response as? HTTPURLResponse {
+            let range = 400...599
+            if range ~= httpResponse.statusCode {
+                var desc = "Invoke Connection call failed with the following status code " + String(httpResponse.statusCode)
+
+                if let safeData = data {
+                    desc = String(decoding: safeData, as: UTF8.self)
+                }
+
+                throw ErrorCodes.APIError(code: httpResponse.statusCode, message: desc).getErrorObject(contextOptions: self.contextOptions)
+            }
+        }
+
+
+        if let safeData = data {
+            let responseData = try JSONSerialization.jsonObject(with: safeData, options: .allowFragments)
+            if responseData is [String: Any] {
+                convertedResponse = try RequestHelpers.parseActualResponseAndUpdateElements(
+                    response: responseData as! [String: Any],
+                    responseBody: config.responseBody ?? [:],
+                    contextOptions: self.contextOptions)
+                errors = RequestHelpers.getInvalidResponseKeys(config.responseBody ?? [:], responseData as! [String : Any], contextOptions: self.contextOptions)
+            } else if responseData is String {
+                stringResponse = responseData as? String
+            }
+        }
+        
+        return (convertedResponse, errors, stringResponse)
+    }
+    
+    func handleCallbacks(isSuccess: Bool, convertedResponse: [String: Any]?, stringResponse: String?, errors: [NSError], errorObject: Error?) {
+        if isSuccess {
+            do {
+                if convertedResponse != nil {
+                    let sanitizedResponse = try ConversionHelpers.removeEmptyValuesFrom(response: convertedResponse!, contextOptions: self.contextOptions)
+
+                    self.callback.onSuccess(sanitizedResponse)
+                } else if stringResponse != nil {
+                    self.callback.onSuccess(stringResponse!)
+                }
+            } catch {
+                self.callback.onFailure(error)
+            }
+        } else {
+            if !errors.isEmpty {
+                let failureResponse: [String: Any] = ["success": convertedResponse ?? [:], "errors": [errors]]
+                self.callback.onFailure(failureResponse)
+            } else {
+                self.callback.onFailure(["errors": [errorObject!]])
+            }
         }
     }
 }
--- Tests/skyflow-iOS-utilTests/skyflow_iOS_connectionUtilTests.swift
+++ Tests/skyflow-iOS-utilTests/skyflow_iOS_connectionUtilTests.swift
@@ -5,9 +5,11 @@ import XCTest
 final class skyflow_iOS_connectionUtilTests: XCTestCase {
     
     var soapApiClient: SoapConnectionAPIClient! = nil
+    var connectionApiClient: ConnectionAPIClient! = nil
     
     override func setUp() {
         self.soapApiClient = SoapConnectionAPIClient(callback: DemoAPICallback(expectation: XCTestExpectation()), skyflow: Client(Configuration(tokenProvider: DemoTokenProvider())), contextOptions: ContextOptions())
+        self.connectionApiClient = ConnectionAPIClient(callback: DemoAPICallback(expectation: XCTestExpectation()), contextOptions: ContextOptions())
     }
     
     func testSoapProcessResponse() {
@@ -27,7 +29,6 @@ final class skyflow_iOS_connectionUtilTests: XCTestCase {
     }
     
     func testConstructSoapRequestError() {
-        let record = GetByIdRecord(ids: ["one", "two"], table: "table", redaction: "REDACTED")
         let httpResponse = HTTPURLResponse(url: URL(string: "http://example.org")!, statusCode: 500, httpVersion: nil, headerFields: ["x-request-id": "RID"])!
         
         let serverError = "<error>Internal Server Error</error>"
@@ -51,6 +52,70 @@ final class skyflow_iOS_connectionUtilTests: XCTestCase {
             XCTAssertNotNil(result.httpBody)
         } catch {
             XCTFail(error.localizedDescription)
+        }
+    }
+    
+    func testConnectionRequestSession() {
+        let config = ConnectionConfig(connectionURL: "https://example.org", method: .POST)
+        let token = "dummy_token"
+        
+        do {
+            let (request, session) = try self.connectionApiClient.getRequestSession(config: config, token: token)
+            
+            XCTAssertEqual(request.allHTTPHeaderFields!["x-skyflow-authorization"], token)
+            XCTAssertEqual(request.url?.absoluteString, config.connectionURL)
+        } catch {
+            XCTFail(error.localizedDescription)
+        }
+    }
+    
+    func testConnectionProcessResponse() {
+        let responseData = ["response": ["message": "success"]]
+        do {
+            let data = try JSONSerialization.data(withJSONObject: responseData, options: .fragmentsAllowed)
+            let response = HTTPURLResponse(url: URL(string: "https://example.org")!, statusCode: 200, httpVersion: "1.1", headerFields: nil)
+            let config = ConnectionConfig(connectionURL: "https://example.org", method: .POST)
+            
+            let (convertedResponse, errors, responseString) = try self.connectionApiClient.processResponse(data: data, response: response, error: nil, config: config)
+            
+            XCTAssertEqual(convertedResponse as! [String: [String: String]], responseData)
+            XCTAssertNil(responseString)
+            XCTAssertEqual(errors.count, 0)
+        } catch {
+            XCTFail(error.localizedDescription)
+        }
+    }
+    
+    func testConnectionProcessResponseFailure() {
+        let responseData = ["response": ["message": "success"]]
+        do {
+            let data = try JSONSerialization.data(withJSONObject: responseData, options: .fragmentsAllowed)
+            let response = HTTPURLResponse(url: URL(string: "https://example.org")!, statusCode: 500, httpVersion: "1.1", headerFields: nil)
+            let config = ConnectionConfig(connectionURL: "https://example.org", method: .POST)
+            
+            let _ = try self.connectionApiClient.processResponse(data: data, response: response, error: nil, config: config)
+            XCTFail("Should have thrown on response code")
+        } catch {
+            if let skyflowErr = error as? SkyflowError {
+                XCTAssertEqual(skyflowErr.code, 500)
+            } else {
+                XCTFail("Throwing unknown error")
+            }
+        }
+    }
+    
+    func testConnectionProcessResponseError() {
+        let responseData = ["response": ["message": "success"]]
+        let netError = NSError(domain: "", code: 1039, userInfo: [NSLocalizedDescriptionKey: "Network Error"])
+        do {
+            let data = try JSONSerialization.data(withJSONObject: responseData, options: .fragmentsAllowed)
+            let response = HTTPURLResponse(url: URL(string: "https://example.org")!, statusCode: 500, httpVersion: "1.1", headerFields: nil)
+            let config = ConnectionConfig(connectionURL: "https://example.org", method: .POST)
+            
+            let _ = try self.connectionApiClient.processResponse(data: data, response: response, error: netError, config: config)
+            XCTFail("Should have thrown on response code")
+        } catch {
+            XCTAssertEqual(error.localizedDescription, netError.localizedDescription)
         }
     }
 }
