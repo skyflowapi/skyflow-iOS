a2b21e1fa56439f64e63004e9a9e22e8a19a5ecf
Tejesh Reddy <tejesh.allampati@skyflow.com>
2022-05-10 15:34:35 +0530

    [SDK-519] add back scenario tests

---
+++ Tests/skyflow-iOS-scenarioTests/skyflow_iOS_getByIdScenarioTests.swift
@@ -0,0 +1,22 @@
+// swiftlint:disable file_length
+import XCTest
+@testable import Skyflow
+
+// swiftlint:disable:next type_body_length
+final class skyflow_iOS_getByIdScenarioTests: XCTestCase {
+    var skyflow: Client!
+    var tokenProvider: TokenProvider!
+    
+    let testData = try! JSONDecoder().decode(TestData.self, from: Data(ProcessInfo.processInfo.environment["TEST_DATA"]!.utf8))
+    
+    override func setUp() {
+        self.skyflow = Client(Configuration(tokenProvider: DemoTokenProvider(), options: Options(logLevel: .DEBUG)))
+        self.tokenProvider = DemoTokenProvider()
+    }
+    
+    override func tearDown() {
+        skyflow = nil
+    }
+
+}
+
---
+++ Tests/skyflow-iOS-scenarioTests/skyflow_iOS_insertScenarioTests.swift
@@ -0,0 +1,32 @@
+// swiftlint:disable file_length
+import XCTest
+@testable import Skyflow
+
+// swiftlint:disable:next type_body_length
+final class skyflow_iOS_insertScenarioTests: XCTestCase {
+    var skyflow: Client!
+    var tokenProvider: TokenProvider!
+    
+    let testData = try! JSONDecoder().decode(TestData.self, from: Data(ProcessInfo.processInfo.environment["TEST_DATA"]!.utf8))
+    
+    override func setUp() {
+        self.skyflow = Client(Configuration(tokenProvider: DemoTokenProvider(), options: Options(logLevel: .DEBUG)))
+        self.tokenProvider = DemoTokenProvider()
+    }
+    
+    override func tearDown() {
+        skyflow = nil
+    }
+    
+    private func getFieldValues() -> [String: String] {
+        var result = [:] as [String: String]
+        for field in testData.VAULT.VALID_FIELDS {
+            result[field.NAME] = field.VALUE
+        }
+        
+        return result
+    }
+    
+
+}
+
