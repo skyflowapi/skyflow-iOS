f1b97a4b6662a789dfc2aa58e6bf72e4952dab7f
skyflow-bharti <bharti.sagar@skyflow.com>
2026-03-08 11:44:59 +0530

    SK-2592-readme-and-sample-changes-for-updating-data-through-elements-in-ios-sdk

---
+++ Samples/UpdateDataUsingElements/UpdateDataUsingElements/ResponseStructs.swift
@@ -0,0 +1,24 @@
+/*
+ * Copyright (c) 2022 Skyflow
+ */
+
+import Foundation
+
+struct SuccessResponse: Codable {
+    let records: [Records]
+}
+
+struct Records: Codable {
+    let fields: Fields?
+    let table: String
+    let skyflow_id: String?
+}
+
+struct Fields: Codable {
+    let cardholder_name: String?
+    let card_number: String?
+    let expiry_month: String?
+    let expiry_year: String?
+    let cvv: String?
+    let skyflow_id: String?
+}
